c509983026d32a6f93152222e25e63af8e81a1af
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-12 17:26:55 -0700

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, {greeting, sum} from classChallenge;
+
+console.log(greeting);
+console.log(sum(25, 50))
+console.log(classChallenge)
---
+++ src/classChallenge.js
@@ -0,0 +1,10 @@
+export var greeting  = 'welcome'
+
+
+export default () => {
+  return `<div>classChallenge</div>`
+}
+
+export function sum(num1, num2) {
+  return num1 + num2
+}
